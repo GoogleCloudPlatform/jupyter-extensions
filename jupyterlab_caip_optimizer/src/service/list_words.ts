860ecc889b359cfabfc8f000ad2b8db636811199
Kyle Pfromer <kpfromer2@gmail.com>
2020-06-15 16:02:54 -0600

    feat(optimizer): add optimizer cookiecutter extension folder

---
+++ jupyterlab_caip_optimizer/src/service/list_words.ts
@@ -0,0 +1,45 @@
+import {ServerConnection} from '@jupyterlab/services';
+import {URLExt} from "@jupyterlab/coreutils";
+
+export interface Word {
+  id: number;
+  word: string;
+}
+
+export interface Words {
+  words: Word[];
+}
+
+export class ListWordsService {
+
+  async listWords(num_items: number): Promise<Words> {
+    return new Promise((resolve, reject) => {
+      let serverSettings = ServerConnection.makeSettings();
+      const requestUrl = URLExt.join(
+        serverSettings.baseUrl, 'optimizer/v1/list');
+      const body = {'num_items': num_items}
+      const requestInit: RequestInit = {
+        body: JSON.stringify(body),
+        method: "POST",
+      };
+      ServerConnection.makeRequest(requestUrl, requestInit, serverSettings
+      ).then((response) => {
+        response.json().then((content) => {
+          if (content.error) {
+            console.error(content.error);
+            reject(content.error);
+            return [];
+          }
+          resolve({
+            words: content.words.map((w: any) => {
+              return {
+                id: w.id,
+                word: w.name,
+              }
+            })
+          });
+        });
+      });
+    });
+  }
+}
