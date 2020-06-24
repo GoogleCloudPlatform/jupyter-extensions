f5890befad22bf20cbc2f013ebda25ecfdbc0fe5
Kyle Pfromer <kpfromer2@gmail.com>
2020-06-24 12:42:44 -0600

    feat: add managed main area widget to application

--- jupyterlab_caip_optimizer/src/index.ts
+++ jupyterlab_caip_optimizer/src/index.ts
@@ -1,41 +1,68 @@
 // Ensure styles are loaded by webpack
 import '../style/index.css';
-
 import {
   JupyterFrontEnd,
-  JupyterFrontEndPlugin
+  JupyterFrontEndPlugin,
 } from '@jupyterlab/application';
+import { Widget } from '@phosphor/widgets';
+import { Store } from 'redux';
+import { store } from './store/store';
+import { watch } from './store/watch';
+import { MainAreaWidget } from './components/main_area_widget';
 
-import {ListWordsWidget} from './components/list_words_widget';
-import {ListWordsService} from './service/list_words';
-
-
-async function activate(
+/**
+ * Opens and closes a widget based on redux store's `view.isVisible` property.
+ * @param reduxStore the redux store with `view.isVisible`.
+ * @param app the jupyter application.
+ * @param widgetComponent the widget to be managed by redux.
+ */
+const createManagedWidget = <
+  WIDGET extends Widget,
+  STORE extends Store<{ view: { isVisible: boolean } }>
+>(
+  reduxStore: STORE,
   app: JupyterFrontEnd,
-) {
-  const listWordsService = new ListWordsService();
-  const listWidget = new ListWordsWidget(listWordsService);
-  listWidget.addClass('optimizer');
-  app.shell.add(listWidget, 'left', {rank: 100});
+  widgetComponent: new (reduxStore: STORE) => WIDGET
+) => {
+  let widget = new widgetComponent(reduxStore);
+  widget.id = 'optimizer:main-area';
 
+  const onChange = watch(
+    reduxStore.getState,
+    state => state.view.isVisible,
+    (previousIsVisible, nextIsVisible) => previousIsVisible === nextIsVisible
+  );
+  reduxStore.subscribe(
+    onChange(isVisible => {
+      if (isVisible) {
+        app.shell.add(widget, 'main');
+        app.shell.activateById(widget.id);
+      } else {
+        widget.close();
+        // Setup component for next open
+        widget = new widgetComponent(reduxStore);
+        widget.id = 'optimizer:main-area';
+      }
+    })
+  );
+};
+
+async function activate(app: JupyterFrontEnd) {
+  // Create main area widget
+  createManagedWidget(store, app, MainAreaWidget);
 }
-
 
 /**
  * The JupyterLab plugin.
  */
 const ListWordsPlugin: JupyterFrontEndPlugin<void> = {
   id: 'caip-optimizer',
-  requires: [
-  ],
+  requires: [],
   activate: activate,
-  autoStart: true
+  autoStart: true,
 };
-
 
 /**
  * Export the plugin as default.
  */
-export default [
-  ListWordsPlugin,
-];
+export default [ListWordsPlugin];
