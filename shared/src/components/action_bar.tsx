c2c9b148013fa9c9eec15b36d98df634ba102556
josiegarza <josiegarza@google.com>
2020-06-24 10:06:37 -0600

    added shared components

---
+++ shared/src/components/action_bar.tsx
@@ -0,0 +1,37 @@
+import * as csstips from 'csstips';
+import * as React from 'react';
+import { style } from 'typestyle';
+import { css } from 'gcp-jupyterlab-shared';
+
+interface Props {
+  children?: React.ReactNode;
+  closeLabel?: string;
+  onClick: () => void;
+}
+
+const actionBar = style({
+  padding: '16px',
+  $nest: {
+    '&>*': {
+      marginLeft: '16px',
+    },
+  },
+  ...csstips.horizontal,
+  ...csstips.endJustified,
+});
+
+/** Funtional Component for defining an action bar with buttons. */
+export function ActionBar(props: Props) {
+  return (
+    <div className={actionBar}>
+      <button
+        type="button"
+        className={css.button}
+        onClick={props.onClick}
+      >
+        {props.closeLabel || 'Close'}
+      </button>
+      {props.children}
+    </div>
+  );
+}
