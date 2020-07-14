3893191d49108ec6171b6a38cc053feee23542a1
Yaminah Qureshi <yaminah@google.com>
2020-07-14 16:11:10 +0000

    adding machine type drop down select component

---
+++ jupyterlab_gcedetails/src/components/machine_type_select.tsx
@@ -0,0 +1,251 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as React from 'react';
+import { stylesheet } from 'typestyle';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import ChevronRightRoundedIcon from '@material-ui/icons/ChevronRightRounded';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { Option, MACHINE_TYPES } from '../data';
+
+export const STYLES = stylesheet({
+  itemGroup: {
+    overflowY: 'auto',
+    height: '240px',
+    width: '233px',
+  },
+  menuContainer: {
+    paddingTop: '8px',
+    paddingBottom: '8px',
+    width: '468px',
+  },
+});
+
+/* Item component that the user can select on the menu */
+
+interface ItemProps {
+  option: Option;
+  onSelect: (o: Option) => void;
+  selected: boolean;
+}
+
+export function Item(props: ItemProps) {
+  const { option, onSelect, selected } = props;
+
+  return (
+    <ListItem onClick={e => onSelect(option)} button selected={selected}>
+      <ListItemText primary={option.value} secondary={option.text} />
+    </ListItem>
+  );
+}
+
+/* Header item component that, on hover, changes the options the user can select 
+from on the menu */
+
+interface HeaderItemProps {
+  label: string;
+  onHover: () => void;
+}
+
+interface HeaderItemState {
+  isHovering: boolean;
+}
+
+class HeaderItem extends React.Component<HeaderItemProps, HeaderItemState> {
+  constructor(props: HeaderItemProps) {
+    super(props);
+
+    this.state = {
+      isHovering: false,
+    };
+  }
+
+  render() {
+    const { label, onHover } = this.props;
+
+    return (
+      <div onMouseEnter={() => onHover()}>
+        <ListItem button>
+          <ListItemText primary={label} />
+          <ChevronRightRoundedIcon />
+        </ListItem>
+      </div>
+    );
+  }
+}
+
+/* Menu body component */
+
+interface MenuBodyProps {
+  onSelect: (o: Option) => void;
+  selectedOption: Option;
+}
+
+interface MenuBodyState {
+  displayedOptions: Option[];
+}
+
+class MenuBody extends React.Component<MenuBodyProps, MenuBodyState> {
+  constructor(props: MenuBodyProps) {
+    super(props);
+
+    this.state = {
+      displayedOptions: MACHINE_TYPES[0].configurations,
+    };
+  }
+
+  /* Displays a menu of header items that, on hover, change the options the user 
+        can select on the menu */
+  private headersList() {
+    return (
+      <div className={STYLES.itemGroup}>
+        <List dense={true} disablePadding={true}>
+          <HeaderItem
+            key={MACHINE_TYPES[0].base.value}
+            label={MACHINE_TYPES[0].base.text}
+            onHover={() =>
+              this.setState({
+                displayedOptions: MACHINE_TYPES[0].configurations,
+              })
+            }
+          />
+          <Divider style={{ marginTop: 4, marginBottom: 4 }} />
+          {MACHINE_TYPES.slice(1).map(machineType => (
+            <HeaderItem
+              key={machineType.base.value}
+              label={machineType.base.text}
+              onHover={() =>
+                this.setState({ displayedOptions: machineType.configurations })
+              }
+            />
+          ))}
+        </List>
+      </div>
+    );
+  }
+
+  /* Displays a list of options that the user can select from */
+  private optionsList() {
+    const { displayedOptions } = this.state;
+    const { onSelect, selectedOption } = this.props;
+
+    return (
+      <div className={STYLES.itemGroup}>
+        <List dense={true} disablePadding={true}>
+          {displayedOptions.map(o => (
+            <Item
+              key={o.value}
+              option={o}
+              onSelect={onSelect}
+              selected={o.value === selectedOption.value}
+            />
+          ))}
+        </List>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+        <Paper elevation={5} square>
+          <div className={STYLES.menuContainer}>
+            <Grid container spacing={0} alignItems="center">
+              <Grid item xs>
+                {this.headersList}
+              </Grid>
+              <Divider orientation="vertical" flexItem />
+              <Grid item xs>
+                {this.optionsList()}
+              </Grid>
+            </Grid>
+          </div>
+        </Paper>
+    );
+  }
+}
+
+/* Menu component */
+
+interface MenuProps {
+  onChange?: (value: string) => void;
+}
+
+interface MenuState {
+  visible: boolean;
+  value: Option;
+}
+
+export class Menu extends React.Component<MenuProps, MenuState> {
+  constructor(props: MenuProps) {
+    super(props);
+
+    this.state = {
+      visible: false,
+      value: MACHINE_TYPES[0].configurations[0],
+    };
+  }
+
+  private displayValue(option: Option) {
+    return `${option.value} (${option.text})`;
+  }
+
+  private toggleMenuBody() {
+    this.setState({ visible: !this.state.visible });
+  }
+
+  private selectOption(newValue: Option) {
+    this.setState({
+      value: newValue,
+      visible: false,
+    });
+
+    this.props.onChange(newValue.value as string);
+  }
+
+  render() {
+    const { visible, value } = this.state;
+
+    return (
+      <div>
+        <OutlinedInput
+          value={this.displayValue(value)}
+          margin="dense"
+          fullWidth={true}
+          readOnly={true}
+          onClick={e => this.toggleMenuBody()}
+          endAdornment={
+            <InputAdornment variant="outlined" position="end">
+              <ArrowDropDownIcon />
+            </InputAdornment>
+          }
+        />
+        {visible && (
+          <MenuBody
+            onSelect={newValue => this.selectOption(newValue)}
+            selectedOption={value}
+          />
+        )}
+      </div>
+    );
+  }
+}
--- jupyterlab_gcedetails/src/data.ts
+++ jupyterlab_gcedetails/src/data.ts
@@ -215,6 +215,141 @@ export const machineTypes: MachineTypeConfigurations[] = [
   },
 ];
 
+/* CPU to Memory mappings for the Compute Engine machine types */
+export interface MachineTypeConfiguration {
+  base: Option
+  configurations: Option[];
+}
+
+export const MACHINE_TYPES: MachineTypeConfiguration[] = [
+  {
+    base: {
+      value: 'n1-standard-',
+      text: 'N1 standard',
+    },
+    configurations: [
+      { value: 'n1-standard-1', text: '1 vCPUs, 3.75 GB RAM' },
+      { value: 'n1-standard-2', text: '2 vCPUs, 7.5 GB RAM' },
+      { value: 'n1-standard-4', text: '4 vCPUs, 15 GB RAM' },
+      { value: 'n1-standard-8', text: '8 vCPUs, 30 GB RAM' },
+      { value: 'n1-standard-16', text: '16 vCPUs, 60 GB RAM' },
+      { value: 'n1-standard-32', text: '32 vCPUs, 120 GB RAM' },
+      { value: 'n1-standard-64', text: '64 vCPUs, 240 GB RAM' },
+      { value: 'n1-standard-96', text: '96 vCPUs, 360 GB RAM' },
+    ],
+  },
+  {
+      base: {
+        value: 'n1-highcpu-',
+        text: 'N1 high-CPU',
+      },
+      configurations: [
+          { value: 'n1-highcpu-2', text: '2 vCPUs, 1.8 GB RAM' },
+          { value: 'n1-highcpu-4', text: '4 vCPUs, 3.6 GB RAM' },
+          { value: 'n1-highcpu-8', text: '8 vCPUs, 7.2 GB RAM' },
+          { value: 'n1-highcpu-16', text: '16 vCPUs, 14.4 GB RAM' },
+          { value: 'n1-highcpu-32', text: '32 vCPUs, 28.8 GB RAM' },
+          { value: 'n1-highcpu-64', text: '64 vCPUs, 57.6 GB RAM' },
+          { value: 'n1-highcpu-96', text: '96 vCPUs, 86 GB RAM' },
+      ],
+    },
+  {
+    base: {
+      value: 'n1-highmem-',
+      text: 'N1 high-memory',
+    },
+    configurations: [
+      { value: 'n1-highmem-2', text: '2 vCPUs, 13 GB RAM' },
+      { value: 'n1-highmem-4', text: '4 vCPUs, 26 GB RAM' },
+      { value: 'n1-highmem-8', text: '8 vCPUs, 52 GB RAM' },
+      { value: 'n1-highmem-16', text: '16 vCPUs, 104 GB RAM' },
+      { value: 'n1-highmem-32', text: '32 vCPUs, 208 GB RAM' },
+      { value: 'n1-highmem-64', text: '64 vCPUs, 416 GB RAM' },
+      { value: 'n1-highmem-96', text: '96 vCPUs, 624 GB RAM' },
+    ],
+  },
+  {
+      base: {
+        value: 'n1-megamem-',
+        text: 'N1 megamem',
+      },
+      configurations: [
+        { value: 'n1-megamem-96', text: '96 vCPUs, 1.4 TB RAM' },
+      ],
+  },
+  {
+      base: {
+        value: 'n1-ultramem-',
+        text: 'N1 ultramem',
+      },
+      configurations: [
+        { value: 'n1-ultramem-40', text: '40 vCPUs, 961 GB RAM' },
+        { value: 'n1-ultramem-80', text: '80 vCPUs, 1922 GB RAM' },
+        { value: 'n1-ultramem-160', text: '160 vCPUs, 3844 GB RAM' },
+      ],
+  },
+  {
+      base: {
+        value: 'n2-highcpu-',
+        text: 'N2 high-CPU',
+      },
+      configurations: [
+        { value: 'n2-highcpu-2', text: '2 vCPUs, 2 GB RAM' },
+        { value: 'n2-highcpu-4', text: '4 vCPUs, 4 GB RAM' },
+        { value: 'n2-highcpu-8', text: '8 vCPUs, 8 GB RAM' },
+        { value: 'n2-highcpu-16', text: '16 vCPUs, 16 GB RAM' },
+        { value: 'n2-highcpu-32', text: '32 vCPUs, 32 GB RAM' },
+        { value: 'n2-highcpu-48', text: '48 vCPUs, 48 GB RAM' },
+        { value: 'n2-highcpu-64', text: '64 vCPUs, 64 GB RAM' },
+        { value: 'n2-highcpu-80', text: '80 vCPUs, 80 GB RAM' },
+      ],
+  },
+  {
+      base: {
+        value: 'n2-highmem-',
+        text: 'N2 high-memory',
+      },
+      configurations: [
+        { value: 'n2-highmem-2', text: '2 vCPUs, 16 GB RAM' },
+        { value: 'n2-highmem-4', text: '4 vCPUs, 32 GB RAM' },
+        { value: 'n2-highmem-8', text: '8 vCPUs, 64 GB RAM' },
+        { value: 'n2-highmem-16', text: '16 vCPUs, 128 GB RAM' },
+        { value: 'n2-highmem-32', text: '32 vCPUs, 256 GB RAM' },
+        { value: 'n2-highmem-48', text: '48 vCPUs, 384 GB RAM' },
+        { value: 'n2-highmem-64', text: '64 vCPUs, 512 GB RAM' },
+        { value: 'n2-highmem-80', text: '80 vCPUs, 640 GB RAM' },
+      ],
+  },
+  {
+      base: {
+        value: 'n2-standard-',
+        text: 'N2 standard',
+      },
+      configurations: [
+        { value: 'n2-standard-2', text: '2 vCPUs, 8 GB RAM' },
+        { value: 'n2-standard-4', text: '4 vCPUs, 16 GB RAM' },
+        { value: 'n2-standard-8', text: '8 vCPUs, 32 GB RAM' },
+        { value: 'n2-standard-16', text: '16 vCPUs, 64 GB RAM' },
+        { value: 'n2-standard-32', text: '32 vCPUs, 128 GB RAM' },
+        { value: 'n2-standard-48', text: '48 vCPUs, 192 GB RAM' },
+        { value: 'n2-standard-64', text: '64 vCPUs, 256 GB RAM' },
+        { value: 'n2-standard-80', text: '80 vCPUs, 320 GB RAM' },
+      ],
+  }, 
+  {
+      base: {
+        value: 'm1-ultramem-',
+        text: 'Memory-optimized',
+      },
+      configurations: [
+        { value: 'm1-ultramem-40', text: '40 vCPUs, 961 GB RAM' },
+        { value: 'm1-ultramem-80', text: '80 vCPUs, 1922 GB RAM' },
+        { value: 'm1-ultramem-96', text: '96 vCPUs, 1.4 TB RAM' },
+        { value: 'm1-ultramem-160', text: '160 vCPUs, 3844 GB RAM' },
+      ],
+  },   
+];
+
 /* Class names applied to the component. */
 export const STYLES = stylesheet({
   container: {
