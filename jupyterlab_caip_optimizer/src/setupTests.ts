4975f760e124abb7f86fd2b40ee821aa62c2d6dd
Kyle Pfromer <kpfromer2@gmail.com>
2020-07-17 15:52:32 -0600

    fix: remove canvas mock warnings

--- jupyterlab_caip_optimizer/src/setupTests.ts
+++ jupyterlab_caip_optimizer/src/setupTests.ts
@@ -16,6 +16,8 @@
 
 import * as Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+// Removes canvas mock not found error
+import 'jest-canvas-mock';
 
 Enzyme.configure({
   adapter: new Adapter(),
