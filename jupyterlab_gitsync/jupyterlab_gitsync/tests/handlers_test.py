de5817540e3394d24364561ab2cb1de7f1abc8f2
Ashley Wang <ashleyswang@google.com>
2020-07-29 17:01:14 -0700

    Inital Commit using Cookiecutter

---
+++ jupyterlab_gitsync/jupyterlab_gitsync/tests/handlers_test.py
@@ -0,0 +1,13 @@
+import unittest
+from unittest.mock import Mock, MagicMock, patch
+
+from jupyterlab_cookies import handlers
+
+
+class TestList(unittest.TestCase):
+
+  def testList(self):
+    pass
+
+if __name__ == '__main__':
+  unittest.main()
