1db56356574187d9e2585de912319385b71a8c69
Ashley Wang <ashleyswang@google.com>
2020-07-29 17:37:25 -0700

    add handler requests api for git sync & service class for high-level logic

--- jupyterlab_gitsync/src/index.ts
+++ jupyterlab_gitsync/src/index.ts
@@ -3,39 +3,47 @@ import '../style/index.css';
 
 import {
   JupyterFrontEnd,
-  JupyterFrontEndPlugin
+  JupyterFrontEndPlugin,
+  ILabShell,
 } from '@jupyterlab/application';
 
-import {ListWordsWidget} from './components/list_words_widget';
-import {ListWordsService} from './service/list_words';
+import { IDocumentManager } from '@jupyterlab/docmanager';
+import { IEditorTracker } from '@jupyterlab/fileeditor';
 
+import { FileTracker } from './service/tracker';
+import { GitManager } from './service/git';
+import { GitSyncService } from './service/service';
+import { GitSyncWidget } from './components/panel';
 
 async function activate(
   app: JupyterFrontEnd,
+  manager: IDocumentManager,
+  shell: ILabShell,
+  editor: IEditorTracker
 ) {
-  const listWordsService = new ListWordsService();
-  const listWidget = new ListWordsWidget(listWordsService);
-  listWidget.addClass('jp-CookiesIcon');
-  app.shell.add(listWidget, 'left', {rank: 100});
-  console.log('list widget activated');
-}
+  // const path = './TESTING';
+  const path = '.';
+  const git = new GitManager(path);
+  const files = new FileTracker(editor);
+  const service = new GitSyncService(git, files, editor);
 
+  const widget = new GitSyncWidget(service);
+  widget.addClass('jp-CookiesIcon');
+  app.shell.add(widget, 'left', { rank: 100 });
+  console.log('git widget activated');
+}
 
 /**
  * The JupyterLab plugin.
  */
-const ListWordsPlugin: JupyterFrontEndPlugin<void> = {
-  id: 'cookies:cookies',
-  requires: [
-  ],
+const GitSyncPlugin: JupyterFrontEndPlugin<void> = {
+  id: 'gitsync:gitsync',
+  requires: [IDocumentManager, ILabShell, IEditorTracker],
   activate: activate,
-  autoStart: true
+  autoStart: true,
 };
-
 
 /**
  * Export the plugin as default.
  */
-export default [
-  ListWordsPlugin,
-];
+export default [GitSyncPlugin];
