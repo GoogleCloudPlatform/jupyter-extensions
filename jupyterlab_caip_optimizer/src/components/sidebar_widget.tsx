519a263ba249162c6db4723586ab6f232351d080
Kyle Pfromer <kpfromer2@gmail.com>
2020-08-03 14:46:50 -0600

    refactor: add create study button, add description to sidebar

--- jupyterlab_caip_optimizer/src/components/sidebar_widget.tsx
+++ jupyterlab_caip_optimizer/src/components/sidebar_widget.tsx
@@ -16,7 +16,7 @@
 
 import * as React from 'react';
 import { ReactWidget } from '@jupyterlab/apputils';
-import { Provider, connect } from 'react-redux';
+import { Provider, connect, useDispatch } from 'react-redux';
 import {
   Box,
   Button,
@@ -27,6 +27,8 @@ import {
   TableContainer,
   TableRow,
   Typography,
+  ButtonGroup,
+  Link,
 } from '@material-ui/core';
 import { RootState, AppDispatch } from '../store/store';
 import { Study } from '../types';
@@ -36,6 +38,7 @@ import { prettifyStudyName } from '../service/optimizer';
 import { style } from 'typestyle';
 import { styles } from '../utils/styles';
 import { Loading } from './loading';
+import AddIcon from '@material-ui/icons/Add';
 
 const rowStyle = style({
   $nest: {
@@ -72,6 +75,7 @@ export const Sidebar = ({
   openDashboard,
   openStudy,
 }: SidebarProps) => {
+  const dispatch = useDispatch();
   return (
     <Box
       className={styles.root}
@@ -82,20 +86,55 @@ export const Sidebar = ({
       display="flex"
       flexDirection="column"
     >
-      <Box display="flex" pt={2}>
-        <Box mx="auto">
-          <Typography variant="h4" gutterBottom>
-            Optimizer
-          </Typography>
+      <Box
+        display="flex"
+        alignItems="center"
+        flexWrap="wrap"
+        pt={2}
+        px={1}
+        style={{ justifyContent: 'center' }}
+      >
+        <Typography variant="h6" gutterBottom>
+          Optimizer
+        </Typography>
 
-          <Button
-            color="primary"
-            variant="contained"
-            onClick={() => openDashboard()}
-          >
-            Main Dashboard
-          </Button>
+        {/* Spacing Element */}
+        <Box style={{ flexGrow: 1 }} />
+
+        <Box>
+          <ButtonGroup color="primary" variant="contained">
+            <Button onClick={() => openDashboard()}>Main Dashboard</Button>
+            <Button
+              onClick={() =>
+                dispatch(
+                  setView({
+                    view: 'createStudy',
+                  })
+                )
+              }
+              startIcon={<AddIcon />}
+            >
+              Create Study
+            </Button>
+          </ButtonGroup>
         </Box>
+      </Box>
+
+      <Box my={2} px={1}>
+        <Typography variant="body2">
+          AI Platform Optimizer is a black-box optimization service that helps
+          you tune hyperparameters in complex machine learning (ML) models
+          through studies.{' '}
+          <Link
+            href="https://cloud.google.com/ai-platform/optimizer/docs"
+            color="primary"
+            variant="body2"
+            rel="noopener"
+            target="_blank"
+          >
+            Learn more
+          </Link>
+        </Typography>
       </Box>
 
       {!!error && (
