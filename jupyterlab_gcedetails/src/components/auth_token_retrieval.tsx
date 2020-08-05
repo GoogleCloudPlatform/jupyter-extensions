cd1fcc44b23ab9e3697d5c566964fb5acb3cd8c0
Zi Gao <zi.s.gao@gmail.com>
2020-08-05 02:20:11 +0000

    Changed to reject on error instead of try catch

--- jupyterlab_gcedetails/src/components/auth_token_retrieval.tsx
+++ jupyterlab_gcedetails/src/components/auth_token_retrieval.tsx
@@ -3,28 +3,24 @@ export function authTokenRetrieval(): Promise<string> {
   let authPopup: any;
   const authOrigin = 'https://jupyterlab-interns-sandbox.uc.r.appspot.com';
   return new Promise<string>((resolve, reject) => {
-    try {
-      const listener = (message: MessageEvent) => {
-        if (
-          message.origin === authOrigin &&
-          (message.data['error'] || message.data['credentials'])
-        ) {
-          authPopup.close();
-          window.removeEventListener('message', listener);
-          if (message.data['error']) {
-            throw new Error();
-          }
-          resolve(message.data['credentials']);
+    const listener = (message: MessageEvent) => {
+      if (
+        message.origin === authOrigin &&
+        (message.data['error'] || message.data['credentials'])
+      ) {
+        authPopup.close();
+        window.removeEventListener('message', listener);
+        if (message.data['error']) {
+          reject('Failed to get authentication token');
         }
-      };
-      window.addEventListener('message', listener);
-      authPopup = window.open(
-        `${authOrigin}/authorize`,
-        '_authPopup',
-        'left=100,top=100,width=400,height=400'
-      );
-    } catch (err) {
-      reject(err);
-    }
+        resolve(message.data['credentials']);
+      }
+    };
+    window.addEventListener('message', listener);
+    authPopup = window.open(
+      `${authOrigin}/authorize`,
+      '_authPopup',
+      'left=100,top=100,width=400,height=400'
+    );
   });
 }
