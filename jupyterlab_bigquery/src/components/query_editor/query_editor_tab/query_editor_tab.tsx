062e0e663d53e17d6b88fe849623ee5b286d0f15
Cynthia Jia <cxjia@google.com>
2020-08-12 18:58:44 -0400

    resize with resize detector within editor

--- jupyterlab_bigquery/src/components/query_editor/query_editor_tab/query_editor_tab.tsx
+++ jupyterlab_bigquery/src/components/query_editor/query_editor_tab/query_editor_tab.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import QueryTextEditor from '../query_text_editor/query_text_editor';
+import QueryTextEditor, {
+  QueryResult,
+} from '../query_text_editor/query_text_editor';
 import QueryResults from '../query_text_editor/query_editor_results';
 import {
   QueryId,
@@ -17,12 +19,13 @@ const localStyles = stylesheet({
     ...BASE_FONT,
   },
 });
+import { connect } from 'react-redux';
 
 interface QueryEditorTabProps {
   isVisible: boolean;
   queryId?: string;
   iniQuery?: string;
-  width?: number;
+  queries: { [key: string]: QueryResult };
 }
 
 class QueryEditorTab extends React.Component<QueryEditorTabProps, {}> {
@@ -38,17 +41,26 @@ class QueryEditorTab extends React.Component<QueryEditorTabProps, {}> {
   }
 
   render() {
+    const { queries } = this.props;
+
+    const queryResult = queries[this.queryId];
+    // eslint-disable-next-line no-extra-boolean-cast
+    const showResult = !!queryResult && queryResult.content.length > 0;
+
     return (
       <div className={localStyles.queryTextEditorRoot}>
         <QueryTextEditor
           queryId={this.queryId}
           iniQuery={this.props.iniQuery}
-          width={this.props.width}
         />
-        <QueryResults queryId={this.queryId} />
+        {showResult && <QueryResults queryId={this.queryId} />}
       </div>
     );
   }
 }
 
-export default QueryEditorTab;
+const mapStateToProps = state => {
+  return { queries: state.queryEditorTab.queries };
+};
+
+export default connect(mapStateToProps)(QueryEditorTab);
--- jupyterlab_bigquery/src/components/query_editor/query_editor_tab/query_editor_tab_widget.tsx
+++ jupyterlab_bigquery/src/components/query_editor/query_editor_tab/query_editor_tab_widget.tsx
@@ -2,9 +2,6 @@ import QueryEditorTab from './query_editor_tab';
 import * as React from 'react';
 import { stylesheet } from 'typestyle';
 import { ReduxReactWidget } from '../../../utils/widgetManager/redux_react_widget';
-import { Signal } from '@phosphor/signaling';
-import { Widget } from '@lumino/widgets';
-import { UseSignal } from '@jupyterlab/apputils';
 
 const localStyles = stylesheet({
   panel: {
@@ -25,29 +22,16 @@ export class QueryEditorTabWidget extends ReduxReactWidget {
     this.title.label = `Query Editor ${this.editorNumber}`;
     this.title.closable = true;
   }
-  private resizeSignal = new Signal<this, Widget.ResizeMessage>(this);
-
-  onResize(msg: Widget.ResizeMessage) {
-    this.resizeSignal.emit(msg);
-  }
 
   renderReact() {
     return (
-      <UseSignal signal={this.resizeSignal}>
-        {(_, event: Widget.ResizeMessage) => {
-          const width = event ? event.width : 0;
-          return (
-            <div className={localStyles.panel}>
-              <QueryEditorTab
-                isVisible={this.isVisible}
-                queryId={this.queryId}
-                iniQuery={this.iniQuery}
-                width={width}
-              />
-            </div>
-          );
-        }}
-      </UseSignal>
+      <div className={localStyles.panel}>
+        <QueryEditorTab
+          isVisible={this.isVisible}
+          queryId={this.queryId}
+          iniQuery={this.iniQuery}
+        />
+      </div>
     );
   }
 }
