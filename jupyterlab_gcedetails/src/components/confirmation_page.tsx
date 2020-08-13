cd9aae47a72363685dec46a4d2ee7a894818d225
Yaminah Qureshi <yaminah@google.com>
2020-08-13 20:48:45 +0000

    Fixing styling of error page

--- jupyterlab_gcedetails/src/components/confirmation_page.tsx
+++ jupyterlab_gcedetails/src/components/confirmation_page.tsx
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import * as React from 'react';
 import { Message } from 'gcp_jupyterlab_shared';
 import {
@@ -31,7 +32,8 @@ interface Props {
   onSubmit: () => void;
 }
 
-const INFO_MESSAGE = `Updating your configuration will take 5-10 minutes. During this time you will not be able to access your notebook instance.`;
+const INFO_MESSAGE = `Updating your configuration will take 5-10 minutes. During this 
+time you will not be able to access your notebook instance.`;
 
 function displayConfiguration(
   configuration: HardwareConfiguration,
