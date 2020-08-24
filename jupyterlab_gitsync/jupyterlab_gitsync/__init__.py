d8f6938649acd4a72a53d47c26a1b16adb0e8fe3
Ashley Wang <38049558+ashleyswang@users.noreply.github.com>
2020-08-24 15:30:03 -0700

    Fix indentation to pass tests

--- jupyterlab_gitsync/jupyterlab_gitsync/__init__.py
+++ jupyterlab_gitsync/jupyterlab_gitsync/__init__.py
@@ -12,15 +12,15 @@ def _jupyter_server_extension_paths():
 
 def load_jupyter_server_extension(nb_server_app):
   """
-    Called when the extension is loaded.
-    Args:
-        nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
-    """
-    host_pattern = '.*$'
-    app = nb_server_app.web_app
-    gcp_v1_endpoint = url_path_join(
-      app.settings['base_url'], 'jupyterlab_gitsync', 'v1')
-    app.add_handlers(host_pattern, [
-      (url_path_join(gcp_v1_endpoint, 'sync') + '(.*)', SyncHandler),
-      (url_path_join(gcp_v1_endpoint, 'setup') + '(.*)', SetupHandler)
-    ])
+  Called when the extension is loaded.
+  Args:
+      nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
+  """
+  host_pattern = '.*$'
+  app = nb_server_app.web_app
+  gcp_v1_endpoint = url_path_join(
+    app.settings['base_url'], 'jupyterlab_gitsync', 'v1')
+  app.add_handlers(host_pattern, [
+    (url_path_join(gcp_v1_endpoint, 'sync') + '(.*)', SyncHandler),
+    (url_path_join(gcp_v1_endpoint, 'setup') + '(.*)', SetupHandler)
+  ])
