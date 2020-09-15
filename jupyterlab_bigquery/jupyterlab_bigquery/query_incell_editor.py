4425ebf22751282d4cc8887b032d02dad9a0fa2b
mrznone <whz@google.com>
2020-09-15 18:40:12 +0000

    support/placeholder for unsupported args

--- jupyterlab_bigquery/jupyterlab_bigquery/query_incell_editor.py
+++ jupyterlab_bigquery/jupyterlab_bigquery/query_incell_editor.py
@@ -10,6 +10,11 @@ from ipywidgets import DOMWidget
 module_name = 'bigquery_query_incell_editor'
 module_version = '0.0.1'
 
+UNSUPPORTED_ARGS = [
+    'destination_var', 'max_results', 'dry_run', 'use_legacy_sql',
+    'use_bqstorage_api', 'use_rest_api', 'verbose'
+]
+
 
 class QueryIncellEditor(DOMWidget):
   _model_name = Unicode('QueryIncellEditorModel').tag(sync=True)
@@ -24,6 +29,11 @@ class QueryIncellEditor(DOMWidget):
 
 
 @magic_arguments.magic_arguments()
+@magic_arguments.argument(
+    "destination_var",
+    nargs="?",
+    help=("Unsupported flag.  Placeholder to match bigquery magic."),
+)
 @magic_arguments.argument(
     "--destination_table",
     type=str,
@@ -42,10 +52,47 @@ class QueryIncellEditor(DOMWidget):
     ),
 )
 @magic_arguments.argument(
+    "--max_results",
+    default=None,
+    help=("Unsupported flag.  Placeholder to match bigquery magic."),
+)
+@magic_arguments.argument(
     "--maximum_bytes_billed",
     default=None,
     help=("maximum_bytes_billed to use for executing this query. Defaults to "
           "the context default_query_job_config.maximum_bytes_billed."),
+)
+@magic_arguments.argument(
+    "--dry_run",
+    action="store_true",
+    default=False,
+    help=("Unsupported flag.  Placeholder to match bigquery magic."),
+)
+@magic_arguments.argument(
+    "--use_legacy_sql",
+    action="store_true",
+    default=False,
+    help=("Sets query to use Legacy SQL instead of Standard SQL. Defaults to "
+          "Standard SQL if this argument is not used."),
+)
+@magic_arguments.argument(
+    "--use_bqstorage_api",
+    action="store_true",
+    default=None,
+    help=(
+        "[Deprecated] Unsupported flag.  Placeholder to match bigquery magic."),
+)
+@magic_arguments.argument(
+    "--use_rest_api",
+    action="store_true",
+    default=False,
+    help=("Unsupported flag.  Placeholder to match bigquery magic."),
+)
+@magic_arguments.argument(
+    "--verbose",
+    action="store_true",
+    default=False,
+    help=("Unsupported flag.  Placeholder to match bigquery magic."),
 )
 @magic_arguments.argument(
     "--params",
@@ -58,15 +105,21 @@ class QueryIncellEditor(DOMWidget):
           "reference can be made by including a '$' before the variable "
           "name (ex. $my_dict_var)."),
 )
-@magic_arguments.argument(
-    "--use_legacy_sql",
-    action="store_true",
-    default=False,
-    help=("Sets query to use Legacy SQL instead of Standard SQL. Defaults to "
-          "Standard SQL if this argument is not used."),
-)
 def _cell_magic(line, query=None):
   args = magic_arguments.parse_argstring(_cell_magic, line)
+
+  args_dict = vars(args)
+
+  active_unsupport_args = []
+  for unsupport_arg in UNSUPPORTED_ARGS:
+    if args_dict[unsupport_arg] is not None and args_dict[
+        unsupport_arg] is not False:
+      active_unsupport_args.append(unsupport_arg)
+
+  if len(active_unsupport_args) > 0:
+    active_unsupport_args.sort()
+
+    print(F"Unsupported args: {', '.join(active_unsupport_args)}")
 
   params = None
 
