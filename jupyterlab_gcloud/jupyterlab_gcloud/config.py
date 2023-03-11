2e8ec69d7eaf35f2acd882ace35e6d58a814569b
Omar Jarjur <ojarjur@google.com>
2023-03-11 00:07:01 +0000

    Add helper methods for using gcloud in a Jupyter config file

---
+++ jupyterlab_gcloud/jupyterlab_gcloud/config.py
@@ -0,0 +1,64 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import subprocess
+
+from jupyterlab_gcloud.tokenrenewer import CommandTokenRenewer
+
+
+def get_gcloud_config(field):
+  """Helper method that invokes the gcloud config helper."""
+  p = subprocess.run(
+    ['gcloud', 'config', 'config-helper', f'--format=value({field})'],
+    stdin=subprocess.DEVNULL, capture_output=True, check=True, encoding='UTF-8')
+  return p.stdout.strip()
+
+
+def gcp_project():
+  """Helper method to get the project configured through gcloud"""
+  return get_gcloud_config('configuration.properties.core.project')
+
+
+def gcp_project_number():
+  """Helper method to get the project number for the project configured through gcloud"""
+  project = gcp_project()
+  p = subprocess.run(
+      ['gcloud', 'projects', 'describe', project, '--format="value(projectNumber)"'],
+    stdin=subprocess.DEVNULL, capture_output=True, check=True, encoding='UTF-8')
+  return p.stdout.strip()
+  
+
+def gcp_region():
+  """Helper method to get the project configured through gcloud"""
+  region = get_gcloud_config('configuration.properties.dataproc.region')
+  if not region:
+    region = get_gcloud_config('configuration.properties.compute.region')
+  return region
+
+
+def gcp_kernel_gateway_url():
+  """Helper method to return the kernel gateway URL for the configured project and region."""
+  project = gcp_project_number()
+  region = gcp_region()
+  return f'https://{project}-dot-{region}.kernels.googleusercontent.com'
+
+
+def configure_gateway_client(c):
+  """Helper method for configuring the given Config object to use the GCP kernel gateway."""
+  c.GatewayClient.url = gcp_kernel_gateway_url()
+  c.GatewayClient.gateway_token_renewer_class = CommandTokenRenewer
+  c.CommandTokenRenewer.token_command = [
+    'gcloud', 'config', 'config-helper', '--format=value(credential.access_token)']
+  c.GatewayClient.auth_scheme = 'Bearer'
+  c.GatewayClient.headers = '{"Cookie": "_xsrf=XSRF", "X-XSRFToken": "XSRF"}'
---
+++ jupyterlab_gcloud/jupyterlab_gcloud/tokenrenewer.py
@@ -0,0 +1,82 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
+import subprocess
+import typing
+
+
+from abc import abstractmethod
+from traitlets import Int, List
+from jupyter_server.gateway.gateway_client import GatewayTokenRenewerBase
+
+
+class CachedTokenRenewerBase(GatewayTokenRenewerBase):
+  """Token renewer base class that only renews the token after a specified timeout."""
+
+  token_lifetime_seconds = Int(
+    default_value=300,
+    config=True,
+    help="""Time (in seconds) to wait between successive token renewals.""",
+  )
+
+  @abstractmethod
+  def force_new_token(
+      self,
+      auth_header_key: str,
+      auth_scheme: typing.Union[str, None],
+      **kwargs: typing.Any,
+  ):
+    pass
+
+  _created = datetime.datetime.min
+
+  def get_token(
+      self,
+      auth_header_key: str,
+      auth_scheme: typing.Union[str, None],
+      auth_token: str,
+      **kwargs: typing.Any,
+  ):
+    current_time = datetime.datetime.now()
+    duration = (current_time - self._created).total_seconds()
+    if (not auth_token) or (duration > self.token_lifetime_seconds):
+      auth_token = self.force_new_token(auth_header_key, auth_scheme, **kwargs)
+      self._created = datetime.datetime.now()
+
+    return auth_token
+
+
+class CommandTokenRenewer(CachedTokenRenewerBase):
+  """Token renewer that invokes an external command to generate the token."""
+
+  token_command = List(
+    default_value=[],
+    config=True,
+    help="""External command run to generate auth tokens.""",
+  )
+
+  def force_new_token(
+      self,
+      auth_header_key: str,
+      auth_scheme: typing.Union[str, None],
+      **kwargs: typing.Any,
+  ):
+    p = subprocess.run(
+      self.token_command,
+      stdin=subprocess.DEVNULL,
+      capture_output=True,
+      check=True,
+      encoding='UTF-8')
+    return p.stdout.strip()
