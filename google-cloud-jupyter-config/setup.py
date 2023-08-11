c2ffa7f17870dd4d36fee229a6ffaefbeb232a94
Omar Jarjur <ojarjur@google.com>
2023-08-10 17:59:08 -0700

    Bump google-cloud-jupyter-config version

--- google-cloud-jupyter-config/setup.py
+++ google-cloud-jupyter-config/setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
   name="google-cloud-jupyter-config",
   author="Google, Inc.",
-  version="0.0.3",
+  version="0.0.4",
   description="Jupyter configuration utilities using gcloud",
   long_description=long_description,
   long_description_content_type="text/markdown",
