52e74949105d561a2538b9ac5d831d963d1cb41c
tim-u <tima1000@gmail.com>
2026-08-27 03:19:48 +0000

    Setting the Dataproc traits for when kernels-mixer is not used with the Dataproc Jupyter plugin Added code improvements and minor fixes

--- google-cloud-jupyter-config/google/cloud/jupyter_config/config.py
+++ google-cloud-jupyter-config/google/cloud/jupyter_config/config.py
@@ -256,8 +256,20 @@ def configure_gateway_client(c):
     c.ServerApp.kernel_websocket_connection_class = (
         "google.cloud.jupyter_config.websockets.DataprocGatewayWebSocketConnection"
     )
+    c.ServerApp.kernel_spec_manager_class = (
+        "google.cloud.jupyter_config.managers.DataprocGatewayKernelSpecManager"
+    )
+    c.ServerApp.kernel_manager_class = (
+        "google.cloud.jupyter_config.managers.DataprocGatewayMappingKernelManager"
+    )
     c.DelegatingWebsocketConnection.remote_websocket_connection_class = (
         "google.cloud.jupyter_config.websockets.DataprocGatewayWebSocketConnection"
+    )
+    c.MixingKernelSpecManager.remote_kernel_spec_manager_class = (
+        "google.cloud.jupyter_config.managers.DataprocGatewayKernelSpecManager"
+    )
+    c.MixingMappingKernelManager.remote_kernel_manager_class = (
+        "google.cloud.jupyter_config.managers.DataprocGatewayMappingKernelManager"
     )
 
 
